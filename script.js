af5fb8519954c88b86e6929528cb606aaeff8cb9
gerkharrington <gerkharrington@gmail.com>
2026-09-19 12:07:40 +1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* -------------------[ Site Data ]------------------- */
 /* Site version */
-const site_version = "0.20.1";
+const site_version = "0.20.2";
 
 /* Untitled text default */
 const editor_untitled = "Untitled";
